85502ad85692f8deb97ade44c0d64b85783672b7
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-20 18:58:00 -0300

    Product

--- src/main/java/com/cursosbjpa/curso/config/TestConfig.java
+++ src/main/java/com/cursosbjpa/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursosbjpa.curso.entities.Category;
 import com.cursosbjpa.curso.entities.Order;
+import com.cursosbjpa.curso.entities.Product;
 import com.cursosbjpa.curso.entities.User;
 import com.cursosbjpa.curso.entities.enums.OrderStatus;
 import com.cursosbjpa.curso.repositories.CategoryRepository;
 import com.cursosbjpa.curso.repositories.OrderRepository;
+import com.cursosbjpa.curso.repositories.ProductRepository;
 import com.cursosbjpa.curso.repositories.UserRepository;
 
 @Configuration
@@ -27,7 +29,8 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;	
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	
 	
@@ -39,6 +42,16 @@ public class TestConfig implements CommandLineRunner {
 		Category cat3 = new Category(null, "Computers"); 
 
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+				
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		
 		
 		User u1 = new User(null, "Maria Brown", "maira@gmail.com", "988888888", "123456");
---
+++ src/main/java/com/cursosbjpa/curso/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.cursosbjpa.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursosbjpa.curso.entities.Product;
+import com.cursosbjpa.curso.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
