a57abd93a5faa7fc429c10e45d0f3121bf14e2af
RafaelSaCa <raffaellsaca@gmail.com>
2022-03-22 21:18:02 -0300

    Payment, one-to-one associação

--- src/main/java/com/cursosbjpa/curso/config/TestConfig.java
+++ src/main/java/com/cursosbjpa/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursosbjpa.curso.entities.Category;
 import com.cursosbjpa.curso.entities.Order;
 import com.cursosbjpa.curso.entities.OrderItem;
+import com.cursosbjpa.curso.entities.Payment;
 import com.cursosbjpa.curso.entities.Product;
 import com.cursosbjpa.curso.entities.User;
 import com.cursosbjpa.curso.entities.enums.OrderStatus;
@@ -84,6 +85,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
+		
 	}
 	
 	
